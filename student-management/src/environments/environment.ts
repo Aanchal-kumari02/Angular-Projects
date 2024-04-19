7ba0878aab9c6e19c972aa442c742bf3023cb609
Aanchal-kumari02 <aanchal.kumari193@gmail.com>
2024-04-19 22:56:45 +0530

    angular fire install

--- student-management/src/environments/environment.ts
+++ student-management/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+   firebaseConfig : {
+    apiKey: "AIzaSyDU9LJj6fmE09ni66f8nHfUfVnUuR5GQ3I",
+    authDomain: "student-management-syste-d1841.firebaseapp.com",
+    databaseURL: "https://student-management-syste-d1841-default-rtdb.firebaseio.com",
+    projectId: "student-management-syste-d1841",
+    storageBucket: "student-management-syste-d1841.appspot.com",
+    messagingSenderId: "733522356431",
+    appId: "1:733522356431:web:7a4b3cda583b7ae810efba"
+  },
   production: false
 };
 
