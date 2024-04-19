48470aad19bc3e5be7de61da8b18aceb8d46202b
Aanchal-kumari02 <aanchal.kumari193@gmail.com>
2024-04-19 21:18:49 +0530

    firebase

--- notesApp/src/environments/environment.prod.ts
+++ notesApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDU9LJj6fmE09ni66f8nHfUfVnUuR5GQ3I",
+    authDomain: "student-management-syste-d1841.firebaseapp.com",
+    databaseURL: "https://student-management-syste-d1841-default-rtdb.firebaseio.com",
+    projectId: "student-management-syste-d1841",
+    storageBucket: "student-management-syste-d1841.appspot.com",
+    messagingSenderId: "733522356431",
+    appId: "1:733522356431:web:b6ea468561e1d59810efba"
+  }
 };
